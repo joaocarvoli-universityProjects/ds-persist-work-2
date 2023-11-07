1f0693eb5f6c5a0580379ae904d17fd62be4e84d
joaocarvoli <j@>
2023-11-06 22:53:36 -0300

    Setting up spring project

---
+++ project/backend/work02/src/main/kotlin/br/ufc/work02/Work02Application.kt
@@ -0,0 +1,11 @@
+package br.ufc.work02
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Work02Application
+
+fun main(args: Array<String>) {
+	runApplication<Work02Application>(*args)
+}
---
+++ project/backend/work02/src/test/kotlin/br/ufc/work02/Work02ApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.ufc.work02
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Work02ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
